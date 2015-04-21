9ff73b7735e9be89e1b79de1bb9838f86fedc58a
patcain34 <patcain34@gmail.com>
2015-04-20 23:23:59 -0400

    Revert "Power Up Hit Detection"
    This reverts commit f45daa914be5c847d574d1a1e79f1783993092c3.

--- Ship.java
+++ Ship.java
@@ -1,51 +1,42 @@
 //Ship class
 
-import java.awt.geom.Rectangle2D;
+import java.awt.*;
+import java.awt.event.*;
+import java.awt.geom.*;
+import java.util.*;
 
-public class Ship extends Rectangle2D.Double {
-	private static int HEIGHT = 45;
-	private static int WIDTH = 45;
+import javax.swing.*;
+import javax.swing.Timer;
+
+
+
+public class Ship extends Rectangle2D.Double{
+	private static final int HEIGHT = 45;
+	private static final int WIDTH = 45;
 	private double xCoord = 720;
 	private double yCoord = 600;
-
-	public Ship() {
+	public Ship(){
 		super(720, 300, WIDTH, HEIGHT);
 	}
-
-	public void set_xCoord(double newX) {
+	
+	public void set_xCoord(double newX){
 		xCoord = newX;
 	}
-
-	public void set_yCoord(double newY) {
+	public void set_yCoord(double newY){
 		yCoord = newY;
 	}
-
-	public double get_xCoord() {
+	
+	public double get_xCoord(){
 		return xCoord;
 	}
-
-	public double get_yCoord() {
+	public double get_yCoord(){
 		return yCoord;
 	}
-
-	public int get_HEIGHT() {
+	public int get_HEIGHT(){
 		return HEIGHT;
 	}
-
-	public int get_WIDTH() {
+	public int get_WIDTH(){
 		return WIDTH;
 	}
-
-	// Addition for PowerUp Actions
-	public void normalSize() {
-		HEIGHT = 45;
-		WIDTH = 45;
-	}
-
-	// Addition for PowerUp Actions
-	public void shrinkShip() {
-		HEIGHT = 25;
-		WIDTH = 25;
-	}
-
+	
 }
